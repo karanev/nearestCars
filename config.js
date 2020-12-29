048fff65cf3d15e071f6feaa32f011258640c1ca
Karan Chaudhary <karan.chaudhary@simsim.in>
2020-12-29 20:19:27 +0530

    changed port in config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     DB_URL: 'mongodb+srv://nearestCars:nearestCars@cluster0-edzww.mongodb.net/nearestCars?retryWrites=true&w=majority',
     MAX_DISTANCE: 200,
+    PORT: 3000,
 };
